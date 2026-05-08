e0585ba1004ab040f779d255754829c7db7fe3e8
youssef <youssef.m.m.abdallah@gmail.com>
2026-05-08 16:36:35 +0300

    updated .gitignore to include data directory and adjusted BASE_DIR in ParquetArchiver along with hadoop dependency in pom file

--- central-station/src/main/java/com/example/storage/parquet/ParquetArchiver.java
+++ central-station/src/main/java/com/example/storage/parquet/ParquetArchiver.java
@@ -22,7 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ParquetArchiver implements AutoCloseable {
 
     private static final int BATCH_SIZE = 10_000;
-    private static final String BASE_DIR   = "/data/parquet";
+    private static final String BASE_DIR = "./data/parquet";
 
     private final Schema schema;
 
